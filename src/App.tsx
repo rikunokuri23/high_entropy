8293037e90c2985176452cad83b64a248bd0b37b
riku <riku023ggl@gmail.com>
2022-06-27 20:50:54 +0900

    add 作成 button

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ShiftGenerate } from './ShiftGenerate';
 import { ArbeitSetting } from './ArbeitSetting';
 import { Calendar } from './Calendar';
+import { Result } from './Result';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<ShiftGenerate/>} />
         <Route path="/arbeitsetting" element={<ArbeitSetting/>} />
         <Route path="/calendar" element={<Calendar/>} />
+        <Route path="/result" element={<Result/>} />
       </Routes>
       </BrowserRouter>
     </div>
--- src/ShiftGenerate.tsx
+++ src/ShiftGenerate.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 export const ShiftGenerate = () => {
+    const navigate = useNavigate();
+    const onClickGenerate = () => {
+        navigate("/result")
+    }
     return (
         <>
         <h1>シフト作成</h1>
+        <button onClick={onClickGenerate}>作成</button>
         </>
     )
 }
