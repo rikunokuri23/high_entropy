3590dc61f0390facdafdf281f10dba2c3fa4063e
GleamDream <ac194601@edu.okinawa-ct.ac.jp>
2022-06-29 13:50:23 +0900

    modify src/components/Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom"
+import { IconContext } from "react-icons"
+import { BsPersonCircle } from 'react-icons/bs'
 
 export const Header = () => {
     return (
         <header className="header" style={headerStyle}>
-            <h3 style={titleStyle}>シフト自動作成</h3>
-            {/* <Link to="/" style={linkStyle}>シフト作成</Link> */}
-            {/* <Link to="/arbeitsetting">アルバイト設定</Link> */}
-            {/* <Link to="/calendar">Calendar</Link> */}
-            <ul style={ ulStyle}>
+            <h3 style={titleStyle}>シフトる</h3>
+            <ul style={ulStyle}>
             <li style={liStyle}><Link to="/" style={linkStyle}>シフト作成</Link></li>
             <li style={liStyle}><Link to="/arbeitsetting" style={linkStyle}>アルバイト設定</Link></li>
             <li style={liStyle}><Link to="/calendar" style={linkStyle}>Calendar</Link></li>
+            <li>
+            <IconContext.Provider value={{style: iconStyle}}><BsPersonCircle /></IconContext.Provider>
+            </li>
             </ul>
         </header>
     )
@@ -18,15 +20,15 @@ export const Header = () => {
 
 const headerStyle = {
     padding: "6px 0px",
-    backgroundColor: "#aaa",
+    backgroundColor: "#B2DFDB",
     display:"flex",
     alignItems:"center"
 }
 const titleStyle = {
-    color: "#555",
+    color: "#000",
+    fontSize: "48px",
     padding: "0px",
     margin:"0px 10px"
-
 }
 
 const ulStyle = {
@@ -34,6 +36,7 @@ const ulStyle = {
     display: "flex",
     marginLeft:"auto"
 }
+
 const liStyle = {
     color: "#555",
     marginRight:"30px",
@@ -42,7 +45,15 @@ const liStyle = {
 
 const linkStyle = {
     color: "#555",
+    fontSize: "20px",
+    fontWeight: "400",
     marginRight:"30px",
     marginLeft:"30px",
     textDecoration:"none"
+}
+
+const iconStyle = {
+    fontSize: "40px",
+    marginRight:"30px",
+    marginLeft:"30px"
 }
